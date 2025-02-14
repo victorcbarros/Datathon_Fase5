e61e6c25885ca32089672ef7b4ee7941cd1e2865
Victor Barros <victorbarros51@gmail.com>
2025-02-14 18:46:27 -0300

    versao com readme2

--- app/app.py
+++ app/app.py
@@ -102,9 +102,8 @@ st.write(
 "No entanto, ainda há desafios a serem superados, especialmente no desenvolvimento social dos alunos e na manutenção do engajamento a longo prazo. O crescimento da participação de alunos mais velhos e a recuperação dos índices acadêmicos em 2024 reforçam a importância de continuar investindo em metodologias inovadoras, suporte individualizado e ações que incentivem a permanência na escola."  
 
 "Com um olhar atento para os desafios e um compromisso contínuo com a educação, a **ONG Passos Mágicos** segue transformando vidas, oferecendo novas oportunidades e construindo um futuro mais promissor para crianças e jovens em situação de vulnerabilidade social. 🚀"
-
-"Este projeto faz parte do projeto Tech Challenge da Pós Tech Data Analytics FIAP e conta como nota da quinta fase da pós-graduação.")
-st.markdown("""O grupo que desenvolveu esse projeto é composto por:
+)
+st.markdown("""Este projeto faz parte do projeto Tech Challenge da Pós Tech Data Analytics FIAP e conta como nota da quinta fase da pós-graduação.O grupo que desenvolveu esse projeto é composto por:
 
 Aelton Pereira de Lacerda
 
